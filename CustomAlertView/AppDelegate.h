7bea310236367ffed453029b573f6bdc8619b04d
Rajesh <rajeshkumarit90@gmail.com>
2014-06-09 10:16:46 +0530

    Initial Commit

---
+++ CustomAlertView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomAlertView
+//
+//  Created by Rajesh on 09/06/14.
+//  Copyright (c) 2014 BlazeDream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CustomAlertView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CustomAlertView
+//
+//  Created by Rajesh on 09/06/14.
+//  Copyright (c) 2014 BlazeDream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
